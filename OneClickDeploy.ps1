9d1c520e0f54631ad700945a2f73e908da5dfa62
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2023-07-21 10:47:50 +0530

    Update OneClickDeploy.ps1

--- OneClickDeploy.ps1
+++ OneClickDeploy.ps1
@@ -54,7 +54,6 @@ try {
     $tenant
 
     Set-AzContext -Tenant $tenant
-    Set-AzureSubscription -SubscriptionName "Microsoft Azure Sponsorship"
     
     # https url for getting workspace details
     $url = "https://management.core.windows.net/subscriptions/" + $SUBSCRIPTION_ID + "/resourceGroups/" + $RG_NAME + "/providers/Microsoft.Databricks/workspaces/" + $WORKSPACE_NAME + "?api-version=2023-02-01"
